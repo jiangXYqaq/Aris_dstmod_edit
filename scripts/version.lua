ca8b94125267faba6f29c0a29fa29a394f7cd546
jiangXYqaq <jiangXYqaq@outlook.com>
2025-05-20 06:21:52 +0800

    1.3.1

--- scripts/version.lua
+++ scripts/version.lua
@@ -1,6 +1,6 @@
 return {
     MAJOR = 1,
-    MINOR = 3, -- Incremented minor version for new features
-    PATCH = 0, -- Reset patch version
-    SUFFIX = "" -- Removed beta suffix as this is a stable release
+    MINOR = 3,
+    PATCH = 1, -- Incremented patch version for bug fixes
+    SUFFIX = "" -- Stable release
 }
