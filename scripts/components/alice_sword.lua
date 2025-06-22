fe8a6e86b70b071ccbc4c9a9a517a9d4a44e880c
jiangXYqaq <jiangXYqaq@outlook.com>
2025-06-22 22:29:23 +0800

    feat:调整光之剑攻击

--- scripts/components/alice_sword.lua
+++ scripts/components/alice_sword.lua
@@ -89,7 +89,7 @@ function Sword:GetDamage(hit_type)
     local damage = 0 
     --1~连射模式 2~能量炮弹 3~EX技能 4~高能激光刀刃
     if mode == 1 then --此处更改了成长后的伤害，因此升级后提升更大，约200%
-        damage = TUNING.ALICE_LIGHTSWORD_DAMAGE * (1 + level * 0.7)
+        damage = TUNING.ALICE_LIGHTSWORD_DAMAGE * (2.0 + level * 1.5)
     elseif mode == 2 then
         -- 更清晰的命中类型区分
         if hit_type == "direct" then
@@ -100,7 +100,7 @@ function Sword:GetDamage(hit_type)
             damage = TUNING.ALICE_LIGHTSWORD_DAMAGE * (1.5 + level * 2.0)
         end
     elseif mode == 3 then
-        damage = TUNING.ALICE_LIGHTSWORD_DAMAGE * (15 + level * 10)
+        damage = TUNING.ALICE_LIGHTSWORD_DAMAGE * (25 + level * 15)
     elseif mode == 4 then
         damage = TUNING.ALICE_LIGHTSWORD_MODE4_PLANAR_DAMAGE_BASE + TUNING.ALICE_LIGHTSWORD_MODE4_PLANAR_DAMAGE_PER_LEVEL * level
     end
